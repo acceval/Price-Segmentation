b868068e9f0c524c481289e35df20b074b283fb9
Chandra Sutrisno Tjhong <chandra.sutrisno@gmail.com>
2021-07-29 15:33:12 +0700

    update pytest

--- test_model.py
+++ test_model.py
@@ -3,13 +3,13 @@ import pytest
 from Model import Model 
 import json
 
-filepath = 'sample_input_file.csv'
+filepath = 'https://github.com/acceval/Price-Segmentation/blob/main/sample_input_file.csv'
 features = ['Customer_Type', 'Customer_Industry', 'Grade', 'Country', 'Destination_Port', 'City_State', 'Shipping_Condition', 'Export/Domestic', 'QUANTITY']
 target_feature = 'Price_Premium'
 index = 'Index'
-price_per_segment = 'price_per_segment.json'
-price_threshold = 'sample_threshold.json'
-price_threshold_power_index = 'sample_threshold_with_power_index.json'
+price_per_segment = 'https://github.com/acceval/Price-Segmentation/blob/main/price_per_segment.json'
+price_threshold = 'https://github.com/acceval/Price-Segmentation/blob/main/sample_threshold.json'
+price_threshold_power_index = 'https://github.com/acceval/Price-Segmentation/blob/main/sample_threshold_with_power_index.json'
 
 model = Model()
 
